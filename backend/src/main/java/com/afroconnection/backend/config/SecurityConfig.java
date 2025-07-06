d5ea8136f4ec1eae12489dc92914560792443ae8
Garreto <jeffsongarreto@hotmail.com>
2025-07-06 16:55:02 -0300

    Atualização da configuração do CORS para produção

--- backend/src/main/java/com/afroconnection/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/afroconnection/backend/config/SecurityConfig.java
@@ -71,7 +71,10 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "afroconnection.vercel.app"
+            ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
